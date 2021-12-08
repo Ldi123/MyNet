12f96f759c0ee63728cb714aca22632d231dbb3c
di <abc@hidi.uu.me>
2021-12-09 00:58:00 +0800

    commit at 20211209

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,7 @@ git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
+
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
