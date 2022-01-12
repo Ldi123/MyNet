eb36af3fcb1fdfc21f24b8e0fa3ee316c2b93008
di <abc@hidi.uu.me>
2022-01-12 11:43:32 +0800

    更新文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,11 +4,11 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   // theme: 'vdoing', // 使用npm包主题
-  theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+  theme: require.resolve('../../theme-vdoing-1.0'), // 使用本地主题
 
   title: "IamDi",
   description: '简洁、易用的博客系统',
-  base: '/MyNet/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/me/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
