a6fd2ebffd3265feb14e8393815fb074d6483364
di <abc@hidi.uu.me>
2022-02-10 17:25:21 +0800

    更新内容或优化功能

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,11 +1,14 @@
 // 插件配置
 module.exports = [
   // 本地插件
-  // [require('./plugins/love-me'), { // 鼠标点击爱心特效
+  // [require('../plugins/love-me'), { // 鼠标点击爱心特效
   //   color: '#11a8cd', // 爱心颜色，默认随机色
   //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
   // }],
 
+	//随机名言
+	["vuepress-plugin-boxx"],
+	
   'vuepress-plugin-baidu-autopush', // 百度自动推送
 
   // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
