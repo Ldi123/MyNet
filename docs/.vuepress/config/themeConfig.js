2532806b54b1700a5d8cc52ba2bbc1c2052025d3
di <abc@hidi.uu.me>
2022-05-30 22:57:31 +0800

    更新内容或优化功能

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -27,7 +27,8 @@ module.exports = {
   //   "http://114.55.7.136:8088/blog/48831ab97cc94279abced695cc18d4a0.jpg",
   //   "http://114.55.7.136:8088/blog/de967fb42715443d80e05b4cdff626cf.jpg",
 		// 'http://114.55.7.136:8088/blog/c439b4b1ca11470c9f78b6ec51066ee4.jpg',
-		'//114.55.7.136:8088/randomImg',
+		// 'http://114.55.7.136:8088/randomImg' // 随机图片，由于浏览器限制https访问http资源，会自动把http转成https,只能以后认证后使用了
+		'https://api.123home.page/api.php?category=visual',
 		'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
     'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
   ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
