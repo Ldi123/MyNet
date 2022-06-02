573569f2010f7fa1a57fbeb95e84f8037f21bc59
di <abc@hidi.uu.me>
2022-06-02 19:43:06 +0800

    更新内容或优化功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,17 @@ const themeConfig = require('./config/themeConfig.js');
 module.exports = {
   // theme: 'vdoing', // 使用npm包主题
   theme: require.resolve('../../theme-vdoing-1.0'), // 使用本地主题
-
+		devServer: {
+			proxy: {
+				'/ali': {
+							target: 'http://114.55.7.136:8088',
+							changeOrigin: true,
+							pathRewrite: {
+											'/ali': ''
+							}
+				},
+			},
+		},
   title: "IamDi",
   description: '简洁、易用的博客系统',
   base: '/MyNet/', // 格式：'/<仓库名>/'， 默认'/'
---
+++ theme-vdoing-1.0/global-components/GetImgs.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<div v-for="(item,index) in imgs" :key="index">
+			<img :src="item.url" />
+		</div>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+	export default {
+		data() {
+			return {
+				imgs: [],
+			}
+		},
+		created() {
+			this.getImgList();
+		},
+		methods:{
+			getImgList(){
+						axios.get('/ali/list').then((response) => { // then 成功之后的回调
+						this.imgs=	response.data.data
+					}).catch(function (error) { //失败时捕获异常
+						console.log(error);
+					}) 
+				},
+			
+		}
+	}
+</script>
+
+<style>
+</style>
