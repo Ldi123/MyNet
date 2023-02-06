f960d2ea6b6fe4dd0b9daf31d65bfc46dff3a769
ld <cc@hidi.uu.me>
2023-02-06 11:26:07 +0800

    更新内容或优化功能

--- docs/.vuepress/config/htmlModules.js
+++ docs/.vuepress/config/htmlModules.js
@@ -19,110 +19,10 @@
  */
 
 module.exports = {
-  homeSidebarB:`<div class="app">
-    <div class="lantern-box">
-      <div class="lantern-left">
-        <div class="lantern">
-          <div class="lines"></div>
-          <div class="lantern-a">
-            <div class="lantern-b">
-              <div class="lantern-t">新</div>
-            </div>
-          </div>
-          <div class="spike spike-a">
-            <div class="spike-c"></div>
-            <div class="spike-b"></div>
-          </div>
-        </div>
-        <div class="lantern">
-          <div class="lines"></div>
-          <div class="lantern-a">
-            <div class="lantern-b">
-              <div class="lantern-t">年</div>
-            </div>
-          </div>
-          <div class="spike spike-a">
-            <div class="spike-c"></div>
-            <div class="spike-b"></div>
-          </div>
-        </div>
-        <div class="lantern">
-          <div class="lines"></div>
-          <div class="lantern-a">
-            <div class="lantern-b">
-              <div class="lantern-t">快</div>
-            </div>
-          </div>
-          <div class="spike spike-a">
-            <div class="spike-c"></div>
-            <div class="spike-b"></div>
-          </div>
-        </div>
-        <div class="lantern">
-          <div class="lines"></div>
-          <div class="lantern-a">
-            <div class="lantern-b">
-              <div class="lantern-t">乐</div>
-            </div>
-          </div>
-          <div class="spike spike-a">
-            <div class="spike-c"></div>
-            <div class="spike-b"></div>
-          </div>
-        </div>
-      </div>
-      <div class="lantern-right">
-        <div class="lantern">
-          <div class="lines"></div>
-          <div class="lantern-a">
-            <div class="lantern-b">
-              <div class="lantern-t">兔</div>
-            </div>
-          </div>
-          <div class="spike spike-a">
-            <div class="spike-c"></div>
-            <div class="spike-b"></div>
-          </div>
-        </div>
-        <div class="lantern">
-          <div class="lines"></div>
-          <div class="lantern-a">
-            <div class="lantern-b">
-              <div class="lantern-t">年</div>
-            </div>
-          </div>
-          <div class="spike spike-a">
-            <div class="spike-c"></div>
-            <div class="spike-b"></div>
-          </div>
-        </div>
-        <div class="lantern">
-          <div class="lines"></div>
-          <div class="lantern-a">
-            <div class="lantern-b">
-              <div class="lantern-t">大</div>
-            </div>
-          </div>
-          <div class="spike spike-a">
-            <div class="spike-c"></div>
-            <div class="spike-b"></div>
-          </div>
-        </div>
-        <div class="lantern">
-          <div class="lines"></div>
-          <div class="lantern-a">
-            <div class="lantern-b">
-              <div class="lantern-t">吉</div>
-            </div>
-          </div>
-          <div class="spike spike-a">
-            <div class="spike-c"></div>
-            <div class="spike-b"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  homeSidebarB:`
+  <center>
+  <img alt="今日诗词" src="https://v2.jinrishici.com/one.svg?font-size=20&color=LightSkyBlue" style="max-width:94%; margin: 25% 0;">
+  </center>
   <!-- 纵向自适应 -->
   <ins class="adsbygoogle"
       style="display:block;padding: 0.95rem;"
@@ -130,170 +30,7 @@ module.exports = {
       data-ad-slot="7802654582"
       data-ad-format="auto"
       data-full-width-responsive="true">
-  </ins>
-  
-  <style scoped>
-  .app{
-   width:100%;min-height:100px;color:#ffaa7f;background: #ffffff;
-  }
-  .lantern-box {
-    position: relative;
-    top: 0px;
-    z-index: 999;
-  }
-   
-  .lantern-left {
-    float: left;
-  }
-   
-  .lantern-right {
-    float: right;
-  }
-   
-  .lantern {
-    position: relative;
-    width: 120px;
-    height: 90px;
-    margin-bottom: 70px;
-    background: #d8000f;
-    background: rgba(216, 0, 15, 0.8);
-    border-radius: 50% 50%;
-    -webkit-transform-origin: 50% -100px;
-    -webkit-animation: swing 3s infinite ease-in-out;
-    box-shadow: -5px 5px 50px 4px rgba(250, 108, 0, 1);
-  }
-   
-  .lantern-a {
-    width: 100px;
-    height: 90px;
-    background: #d8000f;
-    background: rgba(216, 0, 15, 0.1);
-    margin: 12px 8px 8px 8px;
-    border-radius: 50% 50%;
-    border: 2px solid #dc8f03;
-  }
-   
-  .lantern-b {
-    width: 50px;
-    height: 90px;
-    background: #d8000f;
-    background: rgba(216, 0, 15, 0.1);
-    margin: -4px 8px 8px 26px;
-    border-radius: 50% 50%;
-    border: 2px solid #dc8f03;
-  }
-   
-  .lines {
-    position: absolute;
-    top: -20px;
-    left: 60px;
-    width: 3px;
-    height: 20px;
-    background: #dc8f03;
-  }
-   
-  .spike-a {
-    position: relative;
-    width: 5px;
-    height: 20px;
-    margin: -5px 0 0 59px;
-    -webkit-animation: swing 4s infinite ease-in-out;
-    -webkit-transform-origin: 50% -45px;
-    background: #ffa500;
-    border-radius: 0 0 5px 5px;
-  }
-   
-  .spike-b {
-    position: absolute;
-    top: 14px;
-    left: -2px;
-    width: 10px;
-    height: 10px;
-    background: #dc8f03;
-    border-radius: 50%;
-  }
-   
-  .spike-c {
-    position: absolute;
-    top: 18px;
-    left: -2px;
-    width: 10px;
-    height: 35px;
-    background: #ffa500;
-    border-radius: 0 0 0 5px;
-  }
-   
-  .lantern:before {
-    position: absolute;
-    top: -7px;
-    left: 29px;
-    height: 12px;
-    width: 60px;
-    content: " ";
-    display: block;
-    z-index: 999;
-    border-radius: 5px 5px 0 0;
-    border: solid 1px #dc8f03;
-    background: #ffa500;
-    background: linear-gradient(to right, #dc8f03, #ffa500, #dc8f03, #ffa500, #dc8f03);
-  }
-   
-  .lantern:after {
-    position: absolute;
-    bottom: -7px;
-    left: 10px;
-    height: 12px;
-    width: 60px;
-    content: " ";
-    display: block;
-    margin-left: 20px;
-    border-radius: 0 0 5px 5px;
-    border: solid 1px #dc8f03;
-    background: #ffa500;
-    background: linear-gradient(to right, #dc8f03, #ffa500, #dc8f03, #ffa500, #dc8f03);
-  }
-   
-  .lantern-t {
-    font-family: 华文行楷, Arial, Lucida Grande, Tahoma, sans-serif;
-    font-size: 3.2rem;
-    color: #dc8f03;
-    font-weight: bold;
-    line-height: 85px;
-    text-align: center;
-  }
-   
-  .lantern-t, .lantern-box {
-    background: transparent !important;
-  }
-   
-  @keyframes swing {
-    0% {
-      -moz-transform: rotate(-10deg)
-    }
-   
-    50% {
-      -moz-transform: rotate(10deg)
-    }
-   
-    100% {
-      -moz-transform: rotate(-10deg)
-    }
-  }
-   
-  @keyframes swing {
-    0% {
-      -webkit-transform: rotate(-10deg)
-    }
-   
-    50% {
-      -webkit-transform: rotate(10deg)
-    }
-   
-    100% {
-      -webkit-transform: rotate(-10deg)
-    }
-  }
-  </style>`,
+  </ins>`,
   sidebarT:
     `<!--  固定100% * 150px可显示，max-height:150px 未见显示-->
     <ins class="adsbygoogle"
@@ -310,7 +47,8 @@ module.exports = {
           data-ad-client="ca-pub-7828333725993554"
           data-ad-slot="3508773082"
           data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
+          data-full-width-responsive="true">
+      </ins>
       <script>
           (adsbygoogle = window.adsbygoogle || []).push({});
       </script>`,
@@ -331,7 +69,8 @@ module.exports = {
           data-ad-client="ca-pub-7828333725993554"
           data-ad-slot="6620245489"
           data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
+          data-full-width-responsive="true">
+      </ins>
       <script>
           (adsbygoogle = window.adsbygoogle || []).push({});
       </script>`,
@@ -351,7 +90,8 @@ module.exports = {
       <ins class="adsbygoogle"
           style="display:inline-block;max-width:160px;max-height:160px"
           data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="8377369658"></ins>
+          data-ad-slot="8377369658">
+      </ins>
       <script>
           (adsbygoogle = window.adsbygoogle || []).push({});
       </script>
